df24acac0c86eb4beb90f97ee1b5777bf5bf7850
jiuweil <2576101354@qq.com>
2019-08-28 14:32:28 +0800

    上传

---
+++ public/template/precache-manifest.a585f19b32f678e2609cbb7d365809ea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26e8ebaa3c9042e0529ade0169778b25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5da5e9a017fc88c9fe4",
+    "url": "/static/css/2.fbf7db1d.chunk.css"
+  },
+  {
+    "revision": "ce10384a8d90387f1015",
+    "url": "/static/css/3.001e39b8.chunk.css"
+  },
+  {
+    "revision": "17fde7cbece31cafa7ac",
+    "url": "/static/css/main.fe8cf500.chunk.css"
+  },
+  {
+    "revision": "d5da5e9a017fc88c9fe4",
+    "url": "/static/js/2.7d646fc7.chunk.js"
+  },
+  {
+    "revision": "ce10384a8d90387f1015",
+    "url": "/static/js/3.f080e419.chunk.js"
+  },
+  {
+    "revision": "17fde7cbece31cafa7ac",
+    "url": "/static/js/main.fc3e11af.chunk.js"
+  },
+  {
+    "revision": "a70334e51860ec1f4d73",
+    "url": "/static/js/runtime~main.102a4e4a.js"
+  }
+]);
---
+++ routes/api/product.js
@@ -0,0 +1,93 @@
+var express = require('express');
+var router = express.Router();
+let mgdb=require('../../utils/mgdb')
+
+/* GET users listing. */
+router.get('/', (req,res,next)=>{
+  // res.send('product');
+  // console.log(req)
+  let apiname=req.rootParams;
+  let _id=req.query.id
+
+  if(/follow|column|home/.test(apiname)){
+    if(!_id){
+      listFn({req,res,apiname})
+    }else{
+      detailFn({req,res,_id,apiname})
+    }   
+  }else{
+    next()
+  }
+
+  // console.log(1)
+  // console.log(req.rootParams)
+});
+
+
+
+router.get('/:id',(req,res,next)=>{
+  console.log(1)
+  let apiname=req.rootParams;
+  let _id=req.params.id
+  if(/follow|column|home/.test(apiname)){
+    detailFn({req,res,_id,apiname})
+  }else{
+    next()
+  }
+  // console.log(req.params.id)
+  // console.log(req.rootParams)
+
+})
+
+let listFn=({req,res,apiname})=>{
+  let {_page,_limit,q,_sort} = req.query;
+  // console.log(q)
+  let qq = q ? {title:eval('/'+q+'/')} : {}   ;//仔细看 仔细听
+  // sel= sel ? {title:eval('/'+sel+'/')}  : {}
+  // console.log(qq)
+  mgdb({
+    collectionName:apiname
+  },(collection,client)=>{
+    collection.find(qq,{
+      sort:{[_sort]:1},
+      skip: _page * _limit,
+      limit:_limit,
+    }).toArray((err,result)=>{
+      // console.log(result)
+
+      if(!err){
+
+        res.send({err:0,msg:'没有错误',data:result})
+      }else{
+        res.send({err:1,msg:'集合操作失败'})
+      }
+      client.close()
+    })
+  })
+}
+
+let detailFn=({req,res,apiname,_id})=>{
+  mgdb({collectionName:apiname},(collection,client,ObjectID)=>{
+    collection.find({
+      _id: ObjectID(_id)
+    },{
+    }).toArray((err,result)=>{
+      if(!err){
+        if(result.length>0){
+          res.send({err:0,msg:'查询成功',data:result[0]})
+        }else{
+          console.log(1)
+          res.send({err:1,msg:'没有该数据'})
+        }
+        client.close()
+      }else{
+        res.send({err:1,msg:'集合操作失败'})
+      }
+      client.close()
+    })
+  })
+}
+
+
+
+module.exports = router;
